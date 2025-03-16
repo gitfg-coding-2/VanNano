1cd1225fecf373e329491f724e15ef945fa97d25
gitfg-coding-2 <casadeades@gmail.com>
2025-03-16 16:17:13 -0300

    Add files via upload

---
+++ js/First-Warning-On-Console/first-warning-on-console.js
@@ -0,0 +1,3 @@
+// Officialy Created By VanNano
+
+console.log("Please Don't Copy And Paste Unknown Scripts On This Console. Have A Virus O Internet Named Self-XSS And If You Paste A Unknown Script Here You Can Been Infected For This Virus. By gitfg-coding-2.")
